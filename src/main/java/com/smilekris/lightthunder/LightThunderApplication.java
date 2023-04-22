7be64d204a626a4b540577fda7747afdd0589f3e
smile <229161019@qq.com>
2023-04-23 02:07:45 +0800

    【first committed】

---
+++ src/main/java/com/smilekris/lightthunder/LightThunderApplication.java
@@ -0,0 +1,13 @@
+package com.smilekris.lightthunder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightThunderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LightThunderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/smilekris/lightthunder/LightThunderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smilekris.lightthunder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LightThunderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
